fdda5ea3d7f364e0764dc847e0c17cb402d029ee
AngellyD <angelly.delgado1@gmail.com>
2024-09-13 17:53:24 -0500

    Cambios en el formulario, borra datos ingresados

--- Desktop/Laboratorio final de modulo/AD-14-1 The Blog Site Refactor - Group-1.0/ad-l8-1.0/scriptContact.js
+++ Desktop/Laboratorio final de modulo/AD-14-1 The Blog Site Refactor - Group-1.0/ad-l8-1.0/scriptContact.js
@@ -14,6 +14,12 @@ const appendAlert = (message, type) => {
 const alertTrigger = document.getElementById('liveAlertBtn')
 if (alertTrigger) {
   alertTrigger.addEventListener('click', () => {
-    appendAlert('Genial, pronto no pondremos en contacto contigo', 'success')
+    appendAlert('Genial, pronto nos pondremos en contacto contigo', 'success')
+    const nombre = document.querySelector('#name')
+    const correo = document.querySelector('#email')
+    const texto = document.querySelector('#message')
+    nombre.value = ""
+    correo.value = ""
+    texto.value = ""
   })
 }
